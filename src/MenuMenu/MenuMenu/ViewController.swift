e056a32ecb9c8fbf684ba9c752552cad876ad9f2
cheesecat47 <cheesecat47@gmail.com>
2020-05-23 18:26:54 +0900

    Project initialize

---
+++ src/MenuMenu/MenuMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuMenu
+//
+//  Created by refo on 2020/05/23.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
