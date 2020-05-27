b099889df94e495fce31c74136ebbfa7e3a2c15e
cheesecat47 <cheesecat47@gmail.com>
2020-05-27 21:41:50 +0900

    [ui/recipe_list] table view and accept multicheck
    RecipeTableSection.swift is a Model for the table view.
    It contains ingredients name and initialise sections.
    
    RecipeListViewController is a Controller for the model.
    It handles the table view.
    But yet, there isn't the list only containing selected items.

--- src/MenuMenu/MenuMenu/RecipeListViewController.swift
+++ src/MenuMenu/MenuMenu/RecipeListViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class RecipeListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+//    https://stackoverrun.com/ko/q/11646578 여기 참고함.
+    
     @IBOutlet weak var ingredientsSelectView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,37 +19,54 @@ class RecipeListViewController: UIViewController, UITableViewDataSource, UITable
     }
 
     @IBOutlet weak var tableView: UITableView!
+    
     let cellIdentifier: String = "cell"
     
-    let cellTitle: [String] = ["채소류","육류","향신료"]
-    let ingredients: [String:Array<String>] = [
-        "채소류": ["당근","양파","파","마늘"],
-        "육류": ["소고기","양고기","돼지고기","닭고기"],
-        "향신료": ["소금","후추"]
-    ]
+    var recipeTable = RecipeTable()
+    lazy var sections = recipeTable.getSections()
+    
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return ingredients.count
+//        dump("numberOfSections \(sections.count)")
+        return sections.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         // 각 섹션 타이틀 문자열 반환
-        return cellTitle[section]
+//        dump("sections[\(section)]?.title \(String(describing: sections[section]?.title))")
+        return sections[section]?.title
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 테이블 뷰의 한 섹션당 몇 개의 셀을 담을것인지 반환
-        return ingredients[cellTitle[section]]!.count
+//        dump("sections[\(section)]?.items.count \(String(describing: (sections[section]?.items.count)!))")
+        return (sections[section]?.items.count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 각 셀을 반환.
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = sections[indexPath.section]?.items[indexPath.row]
         
-        let text: String = ingredients[cellTitle[indexPath.section]]![indexPath.row]
+        if item!.selected {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         
-        cell.textLabel?.text = text
+        cell.textLabel?.text = item?.name ?? ""
+        
+//        dump("cell: \(cell)")
+        // 테이블 움직일 때 실시간으로 생성.
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var item = sections[indexPath.section]?.items[indexPath.row]
+        item!.selected = !item!.selected
+        dump("item clicked \(String(describing: item))")
+        sections[indexPath.section]?.items[indexPath.row] = item!
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
