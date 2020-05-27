d42c6bfa97495a6890eee65eb7fbca876f23e3a7
cheesecat47 <cheesecat47@gmail.com>
2020-05-27 17:47:33 +0900

    table view

---
+++ src/MenuMenu/MenuMenu/RecipeListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeListViewController.swift
+//  MenuMenu
+//
+//  Created by refo on 2020/05/24.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController {
+
+    @IBOutlet weak var ingredientsSelectView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
