1993b4f7e0afb84eb891a2dab3e5775c703f91d5
cheesecat47 <cheesecat47@gmail.com>
2020-05-30 23:02:10 +0900

    Make two tableViews, do more with lowerTableCell

--- src/MenuMenu/MenuMenu/RecipeListViewController.swift
+++ src/MenuMenu/MenuMenu/RecipeListViewController.swift
@@ -6,21 +6,33 @@
 //  Copyright © 2020 COMP420. All rights reserved.
 //
 
+//    references
+//    https://stackoverrun.com/ko/q/11646578
+//    https://zeddios.tistory.com/169
+//    https://etst.tistory.com/105?category=861730
+
+
 import UIKit
 
 class RecipeListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-//    https://stackoverrun.com/ko/q/11646578 여기 참고함.
     
     @IBOutlet weak var ingredientsSelectView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
+        self.upperTableView.dataSource = self
+        self.upperTableView.delegate = self
+        self.upperTableView.register(UITableViewCell.self, forCellReuseIdentifier: upperCellIdentifier)
+        
+        self.lowerTableView.dataSource = self
+        self.lowerTableView.delegate = self
+        self.lowerTableView.register(RecipeTableLowerTableCell.self, forCellReuseIdentifier: lowerCellIdentifier)
     }
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var upperTableView: UITableView!
+    @IBOutlet weak var lowerTableView: UITableView!
     
-    let cellIdentifier: String = "cell"
+    let upperCellIdentifier: String = "upperCell"
+    let lowerCellIdentifier: String = "lowerCell"
     
     var recipeTable = RecipeTable()
     lazy var sections = recipeTable.getSections()
@@ -28,45 +40,78 @@ class RecipeListViewController: UIViewController, UITableViewDataSource, UITable
     
     func numberOfSections(in tableView: UITableView) -> Int {
 //        dump("numberOfSections \(sections.count)")
-        return sections.count
+        if tableView == upperTableView {
+            return sections.count
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         // 각 섹션 타이틀 문자열 반환
 //        dump("sections[\(section)]?.title \(String(describing: sections[section]?.title))")
-        return sections[section]?.title
+        if tableView == upperTableView {
+            return sections[section]?.title
+        } else {
+            return "검색 결과"
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 테이블 뷰의 한 섹션당 몇 개의 셀을 담을것인지 반환
 //        dump("sections[\(section)]?.items.count \(String(describing: (sections[section]?.items.count)!))")
-        return (sections[section]?.items.count)!
+        if tableView == upperTableView {
+            return (sections[section]?.items.count)!
+        } else {
+            return 10
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 각 셀을 반환.
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = sections[indexPath.section]?.items[indexPath.row]
-        
-        if item!.selected {
-            cell.accessoryType = .checkmark
+        if tableView == upperTableView {
+            let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: upperCellIdentifier, for: indexPath)
+            let item = sections[indexPath.section]?.items[indexPath.row]
+                    
+            if item!.selected {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+                    
+            cell.textLabel?.text = item?.name ?? ""
+                    
+            //        dump("cell: \(cell)")
+                    // 테이블 움직일 때 실시간으로 생성.
+                    
+            return cell
         } else {
-            cell.accessoryType = .none
+            let cell: RecipeTableLowerTableCell = tableView.dequeueReusableCell(withIdentifier: lowerCellIdentifier, for: indexPath) as! RecipeTableLowerTableCell
+
+            cell.lowerCellLabel?.text = "food name"
+            cell.lowerCellImage?.image = UIImage(contentsOfFile: "carrot.jpg")
+                    
+            return cell
         }
-        
-        cell.textLabel?.text = item?.name ?? ""
-        
-//        dump("cell: \(cell)")
-        // 테이블 움직일 때 실시간으로 생성.
-        
-        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        var item = sections[indexPath.section]?.items[indexPath.row]
-        item!.selected = !item!.selected
-        dump("item clicked \(String(describing: item))")
-        sections[indexPath.section]?.items[indexPath.row] = item!
-        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+        if tableView == upperTableView {
+            var item = sections[indexPath.section]?.items[indexPath.row]
+            item!.selected = !item!.selected
+            dump("item clicked \(String(describing: item))")
+            sections[indexPath.section]?.items[indexPath.row] = item!
+            self.upperTableView.reloadRows(at: [indexPath], with: .automatic)
+        } else {
+            dump("lowerCell Clicked!")
+        }
     }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        if tableView == lowerTableView {
+//            return 225
+//        } else {
+//            return 50
+//        }
+//    }
 }
