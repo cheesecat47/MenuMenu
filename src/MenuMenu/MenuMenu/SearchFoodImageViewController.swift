791a8271ea6e2a7c54b6b35708269207d8355fba
cheesecat47 <cheesecat47@gmail.com>
2020-06-17 14:02:14 +0900

    SearchFoodImageViewController - Done

--- src/MenuMenu/MenuMenu/SearchFoodImageViewController.swift
+++ src/MenuMenu/MenuMenu/SearchFoodImageViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 import WebKit
+import JavaScriptCore
+
 class SearchFoodImageViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
     
     var webView: WKWebView!
@@ -15,6 +17,8 @@ class SearchFoodImageViewController: UIViewController, WKUIDelegate, WKNavigatio
         
         let configuration: WKWebViewConfiguration = WKWebViewConfiguration()
         configuration.userContentController = contentController
+        
+        contentController.add(self, name: "native_console_log")
         
         webView = WKWebView(frame: self.view.bounds, configuration: configuration)
         webView.uiDelegate = self
@@ -36,11 +40,30 @@ class SearchFoodImageViewController: UIViewController, WKUIDelegate, WKNavigatio
         
         webView.load(myRequest)
         webView.allowsBackForwardNavigationGestures = true
+        
+        // https://comxp.tistory.com/354
+        // https://bluexmas.tistory.com/818
+//        let context = webView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as! JSContext
+//        let logFunction : @convention(block) (String) -> Void = { (msg:String) in
+//            print("Console : \(msg)")
+//        }
+//        context.objectForKeyedSubscript("console")?.setObject(unsafeBitCast(logFunction, to: AnyObject.self), forKeyedSubscript: "log")
+        let js = "var console = { log: function(message) { webkit.messageHandlers.native_console_log.postMessage(JSON.stringify(message)) } }"
+        webView.evaluateJavaScript(js)
     }
     
+    func native_console_log(didReceive message: WKScriptMessage) {
+        print(NSString(string: "console.log: \(message.body)"))
+    }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "native_console_log" {
+            dump("SearchFoodImageViewController: userContentController: console log")
+            native_console_log(didReceive: message)
+        }
+        
         if message.name == messageHandlerName {
+            dump("SearchFoodImageViewController: userContentController: message")
 //            print("SearchFoodImageViewController: userContentController: message: \(message.body) \(type(of: message.body))")
             // message.body 가 티쳐블 머신 결과물 객체. 이걸로 하고싶은거 하면됨. 이미지 파일 크기와 모델 크기에 따라 쬐에끔 오래걸림
             let msgToArr = message.body as! NSArray
